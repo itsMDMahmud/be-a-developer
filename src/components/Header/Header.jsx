19268117e9d37d2c1cfca065bcb3c39f39c165ab
itsMDMahmud <itsmdmahmud@gmail.com>
2023-03-31 19:38:56 +0600

    changes design

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,12 +1,13 @@
 import React from 'react';
 import './Header.css'
+import img from '../../../public/3135715.png'
 
 const Header = () => {
     return (
         <>
             <div className='header'>
                 <h1 id='header'>Become a Developer</h1>
-                <img src="../../../image/3135715.png" alt="" />            
+                <img src={img} alt="" />            
             </div>
             <hr />
         </>
